ae19e9ac9d6d1e26fb351f8b8d5618d625bde7a4
leejeongwha <ljk2491@nhn.com>
2016-08-23 15:09:48 +0900

    레디스 실습 추가

--- src/test/java/com/naver/test/SpringBootTestApplicationTests.java
+++ src/test/java/com/naver/test/SpringBootTestApplicationTests.java
@@ -1,5 +1,8 @@
 package com.naver.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,15 +21,62 @@ public class SpringBootTestApplicationTests {
 
 	@Test
 	public void text() {
-		redisTemplate.opsForValue().set("user:name", "leejeongwha");
+		redisTemplate.opsForValue().set("user:name", "lee");
+
+		Assert.assertEquals("lee", redisTemplate.opsForValue().get("user:name"));
+
+		redisTemplate.opsForValue().append("user:name", "jeongwha");
 
 		Assert.assertEquals(redisTemplate.opsForValue().get("user:name"), "leejeongwha");
-
-		redisTemplate.opsForValue().append("user:name", "hello");
-
-		Assert.assertEquals(redisTemplate.opsForValue().get("user:name"), "leejeongwhahello");
 
 		System.out.println(redisTemplate.opsForValue().get("user:name"));
 	}
 
+	@Test
+	public void list() {
+		redisTemplate.opsForList().leftPush("my:list:recommand", "java");
+
+		redisTemplate.opsForList().leftPush("my:list:recommand", "javascript");
+
+		System.out.println(redisTemplate.opsForList().range("my:list:recommand", 0, -1));
+	}
+
+	@Test
+	public void set() {
+		redisTemplate.opsForSet().add("my:set:test", "my");
+
+		redisTemplate.opsForSet().add("my:set:test", "name");
+
+		redisTemplate.opsForSet().add("my:set:test", "is");
+
+		redisTemplate.opsForSet().add("my:set:test", "leejeongwha");
+
+		System.out.println(redisTemplate.opsForSet().members("my:set:test"));
+	}
+
+	@Test
+	public void zSet() {
+		redisTemplate.opsForZSet().add("user:ranking", "my", 1);
+
+		redisTemplate.opsForZSet().add("user:ranking", "name", 2);
+
+		redisTemplate.opsForZSet().add("user:ranking", "is", 3);
+
+		redisTemplate.opsForZSet().add("user:ranking", "leejeongwha", 4);
+
+		System.out.println(redisTemplate.opsForZSet().range("user:ranking", 0, -1));
+	}
+
+	@Test
+	public void hash() {
+		String id = "ljk2491";
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("firstName", "Lee");
+		map.put("lastName", "JeongWha");
+		map.put("email", "ljk2491@naver.com");
+
+		redisTemplate.opsForHash().putAll(id, map);
+
+		System.out.println(redisTemplate.opsForHash().get(id, "email"));
+	}
 }
