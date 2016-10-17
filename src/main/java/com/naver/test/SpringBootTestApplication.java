fb0c326b8cb2de92213e71a01daca07e94f0a99e
leejeongwha <ljk2491@nhn.com>
2016-10-17 17:07:51 +0900

    reactive 프로그래밍 샘플 커밋

--- src/main/java/com/naver/test/SpringBootTestApplication.java
+++ src/main/java/com/naver/test/SpringBootTestApplication.java
@@ -1,37 +1,153 @@
 package com.naver.test;
 
+import static org.springframework.web.reactive.function.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.RouterFunctions.route;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.Request;
+import org.springframework.web.reactive.function.Response;
+import org.springframework.web.reactive.function.RouterFunction;
+import org.springframework.web.reactive.function.RouterFunctions;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.ipc.netty.http.HttpServer;
+
+/**
+ * @author NAVER
+ *         https://spring.io/blog/2016/10/05/spring-tips-functional-reactive-
+ *         endpoints-with-spring-framework-5-0
+ */
 @SpringBootApplication
-public class SpringBootTestApplication extends SpringBootServletInitializer {
+public class SpringBootTestApplication {
+	@Bean
+	RouterFunction<?> router(PersonHandler handler) {
+		return route(GET("/persons"), handler::all).and(route(GET("/persons/{id}"), handler::byId));
+	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootTestApplication.class);
+	@Bean
+	HttpServer server(RouterFunction<?> router) {
+		HttpHandler handler = RouterFunctions.toHttpHandler(router);
+		HttpServer httpServer = HttpServer.create(8080);
+		httpServer.start(new ReactorHttpHandlerAdapter(handler));
+		return httpServer;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
 	}
+}
 
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-		return new EmbeddedServletContainerCustomizer() {
-			@Override
-			public void customize(ConfigurableEmbeddedServletContainer container) {
-				ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/400.html");
-				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
-				container.addErrorPages(error400Page, error404Page, error500Page);
-			}
-		};
+@Component
+class PersonHandler {
+	@Autowired
+	private PersonRepository personRepository;
+
+	Response<Flux<Person>> all(Request request) {
+		Flux<Person> flux = Flux.fromStream(personRepository.all());
+		return Response.ok().body(BodyInserters.fromPublisher(flux, Person.class));
+
 	}
+
+	Response<Mono<Person>> byId(Request request) {
+		Optional<String> optional = request.pathVariable("id");
+		return optional.map(id -> personRepository.findById(Long.parseLong(id))).map(person -> Mono.fromFuture(person))
+				.map(mono -> Response.ok().body(BodyInserters.fromPublisher(mono, Person.class)))
+				.orElseThrow(() -> new IllegalStateException("Oops!!"));
+	}
+}
+
+@Component
+class SampleDataCLR implements CommandLineRunner {
+	@Autowired
+	private PersonRepository personRepository;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+		Stream.of("Stephane Maldini", "Arjen Poutsma", "Rossen Stoyanchev", "Sebastien Deleuze", "Josh Long")
+				.forEach(name -> personRepository.save(new Person(name, new Random().nextInt(100))));
+
+		personRepository.findAll().forEach(System.out::println);
+	}
+}
+
+interface PersonRepository extends JpaRepository<Person, Long> {
+
+	CompletableFuture<Person> findById(Long id);
+
+	@Query("select p from Person p")
+	Stream<Person> all();
+}
+
+@Entity
+class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String name;
+	@Column
+	private int age;
+
+	public Person() {
+
+	}
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" + "id=" + id + ", name='" + name + '\'' + ", age=" + age + "}";
+	}
+
 }
