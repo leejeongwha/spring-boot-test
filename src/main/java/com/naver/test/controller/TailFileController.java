2c92abf2f909f5538c7ebc032f7b6a27016c204c
leejeongwha <ljk2491@nhn.com>
2016-10-27 15:33:01 +0900

    tail file 커밋

--- src/main/java/com/naver/test/controller/TailFileController.java
+++ src/main/java/com/naver/test/controller/TailFileController.java
@@ -1,9 +1,15 @@
 package com.naver.test.controller;
 
-import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +22,9 @@ import rx.Observable;
 public class TailFileController {
 	private static final Logger log = LoggerFactory.getLogger(TailFileController.class);
 
+	@Value("${logging.file}")
+	private String fileName;
+
 	@RequestMapping("test")
 	public String test() throws Exception {
 		log.info("현재 시각은 : " + System.currentTimeMillis());
@@ -25,10 +34,14 @@ public class TailFileController {
 	@PostConstruct
 	public void postConstruct() {
 		Runnable task2 = () -> {
-			Observable<String> items = FileObservable.tailer().file("/Users/NAVER/Downloads/spring-boot-test.log")
-					.tailText();
+			Observable<String> items = FileObservable.tailer().file(fileName).tailText();
 			items.subscribe(text -> System.out.println("tail -f log file content : " + text));
 		};
 		new Thread(task2).start();
+	}
+
+	@PreDestroy
+	public void preDestroy() throws IOException {
+		FileUtils.forceDelete(new File(fileName));
 	}
 }
