d4f7d8ffc8a2552800f6ff493fc5cc5e8dafa428
leejeongwha <ljk2491@nhn.com>
2017-01-23 17:14:02 +0900

    단순 requestmapping 제거하고 viewcontroller 추가

--- src/main/java/com/naver/test/WebMvcConfig.java
+++ src/main/java/com/naver/test/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.naver.test;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.naver.test.notice.interceptor.LoginCheckInterceptor;
@@ -11,5 +12,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoginCheckInterceptor());
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
 	}
 }
